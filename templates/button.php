2ae4209897e3513585db0c65371f8317640ae1d4
artikus13 <artikus.sol@gmail.com>
2021-05-31 13:51:03 +0400

    UDP: правки в шаблоне кнопки

--- templates/button.php
+++ templates/button.php
@@ -14,7 +14,4 @@ if ( ! $args ) {
 	class="button button--shortcode button-shortcode-js <?php echo esc_attr( $args['class'] ); ?>"
 	data-window-url="<?php echo esc_attr( $args['url'] ); ?>"
 	data-afb-open
-	data-afb-emails = "<?php echo esc_attr( $args['emails'] ); ?>"
->
-	<?php echo esc_html( $args['label'] ); ?>
-</button>
+	data-afb-emails = "<?php echo esc_attr( $args['emails'] ); ?>"><?php echo esc_html( $args['label'] ); ?></button>
