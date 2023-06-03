09e6b68dd648814639e431195bf73965a0e2d11b
Артем <artikus.sol@gmail.com>
2023-06-03 15:45:26 +0400

    FIX 1.4.4: правки нотисов при апдейте

--- classes/class-updater.php
+++ classes/class-updater.php
@@ -112,9 +112,6 @@ class Updater {
 		if ( is_null( $this->github_response ) ) {
 
 			$request_uri = sprintf( 'https://api.github.com/repos/%s/%s/releases/latest', $this->username, $this->repository );
-			$changelog   = file_get_contents(
-				sprintf( 'https://raw.githubusercontent.com/%s/%s//master/CHANGELOG.md', $this->username, $this->repository )
-			);
 
 			if ( $this->authorize_token ) {
 				$args ['headers'] = [
@@ -139,7 +136,7 @@ class Updater {
 				$this->github_response = [
 					'tag_name'      => $response['tag_name'],
 					'downloaded'    => $assets['download_count'],
-					'updates'       => '<pre style="border:none;width: 100%;word-break: break-all;white-space: pre;">' . $changelog . '</pre>',
+					'updates'       => $response['body'],
 					'last_updated'  => $response['published_at'],
 					'download_link' => $assets['browser_download_url'],
 				];
@@ -187,7 +184,7 @@ class Updater {
 
 		$this->get_repository_data();
 
-		if ( empty( $this->github_response['tag_name'] ) ) {
+		if ( empty( $this->github_response ) ) {
 			return $transient;
 		}
 
